999a581cce49e94a09e7922e570ec79021b76cb7
chrischank <christopherchank@gmail.com>
2021-06-28 14:27:53 +0100

    writing AOI geojson

--- asset_model.R
+++ asset_model.R
@@ -32,7 +32,32 @@ head(Master_street)
 str(Master_street)
 
 # AOI boundaries from OSM
-AOI_las_condes <- osmdata::getbb("Las Condes", featuretype = "boundaries", format_out = "sf_polygon")
-AOI_san_miguel <- osmdata::getbb("San Miguel", featuretype = "boundaries", format_out = "sf_polygon")
-AOI_puente_alto <- osmdata::getbb("Puente Alto", featuretype = "boundaries", format_out = "sf_polygon")
-plot(AOI_san_miguel)
+
+#########################
+
+AOI_Santiago <- osmdata::getbb("Provincia de Santiago", featuretype = "boundaries",
+                               format_out = "sf_polygon") %>%
+  st_transform(AOI_Santiago, crs = 9184)
+
+st_write(AOI_Santiago, dsn = file.path(vector_path, "AOI_Santiago.geojson"), driver = "GeoJSON")
+
+#########################
+
+AOI_las_condes <- osmdata::getbb("Las Condes", featuretype = "boundaries",
+                                 display_name_contains = "Santiago",
+                                 format_out = "sf_polygon") %>%
+  st_transform(AOI_las_condes, crs = 9184)
+
+AOI_san_miguel <- osmdata::getbb("San Miguel", featuretype = "boundaries",
+                                 display_name_contains = "Santiago",
+                                 format_out = "sf_polygon") %>%
+  st_transform(AOI_san_miguel, crs = 9184)
+
+AOI_puente_alto <- osmdata::getbb("Puente Alto", featuretype = "boundaries",
+                                  display_name_contains = "Santiago",
+                                  format_out = "sf_polygon") %>%
+  st_transform(AOI_puente_alto, crs = 9184)
+
+st_write(AOI_las_condes, dsn = file.path(vector_path, "AOI_LC.geojson"), driver = "GeoJSON")
+st_write(AOI_san_miguel, dsn = file.path(vector_path, "AOI_SM.geojson"), driver = "GeoJSON")
+st_write(AOI_puente_alto, dsn = file.path(vector_path, "AOI_PA.geojson"), driver = "GeoJSON")
